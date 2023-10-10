259557b8e31852eeefb1de561613f7041396e85c
Qinan Gao <qinanagao@gmail.com>
2023-10-10 22:04:24 +0100

    add sounds and event listener

--- game.js
+++ game.js
@@ -1,8 +1,22 @@
 buttonColours = ["red", "blue", "green", "yellow"];
 gamePattern = [];
+userClickedPattern = []; //step 4-3
+
+$(".btn").on("click", function () {
+  var userChosenColour = $(this).attr("id"); //step4-2
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern); //step4-4
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
-  return randomChosenColour;
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).animate({ opacity: 0.2 }, 100, function () {
+    $("#" + randomChosenColour).animate({ opacity: 1 }, 100);
+  });
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
