0a5d8bb35573e55ec32ae34df3f2645a0e58a5bf
Qinan Gao <qinanagao@gmail.com>
2023-10-11 22:49:39 +0100

    step 7-2

--- game.js
+++ game.js
@@ -1,22 +1,42 @@
 buttonColours = ["red", "blue", "green", "yellow"];
 gamePattern = [];
-userClickedPattern = []; //step 4-3
+userClickedPattern = [];
 
 $(".btn").on("click", function () {
-  var userChosenColour = $(this).attr("id"); //step4-2
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern); //step4-4
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).animate({ opacity: 0.2 }, 100, function () {
     $("#" + randomChosenColour).animate({ opacity: 1 }, 100);
   });
+  playSound(randomChosenColour);
+  animatePress(randomChosenColour);
+}
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+var keyPressed = false;
+$(document).keydown(function (event) {
+  if (!keyPressed) {
+    keyPressed = true;
+    nextSequence();
+  }
+});
