aa6897a61105c94b374cb55332fdc4a3805a4f42
Qinan Gao <qinanagao@gmail.com>
2023-10-12 22:11:20 +0100

    completed game

--- game.js
+++ game.js
@@ -2,20 +2,56 @@ buttonColours = ["red", "blue", "green", "yellow"];
 gamePattern = [];
 userClickedPattern = [];
 
+var keyPressed = false;
+
+var level = 0;
+$(document).keydown(function (event) {
+  if (!keyPressed) {
+    keyPressed = true;
+    nextSequence();
+  }
+});
+
 $(".btn").on("click", function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(colorIndex) {
+  if (userClickedPattern[colorIndex] === gamePattern[colorIndex]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    new Audio("sounds/wrong.mp3").play();
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).animate({ opacity: 0.2 }, 100, function () {
     $("#" + randomChosenColour).animate({ opacity: 1 }, 100);
   });
+
   playSound(randomChosenColour);
   animatePress(randomChosenColour);
 }
@@ -33,10 +69,8 @@ function animatePress(currentColour) {
   }, 100);
 }
 
-var keyPressed = false;
-$(document).keydown(function (event) {
-  if (!keyPressed) {
-    keyPressed = true;
-    nextSequence();
-  }
-});
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  keyPressed = false;
+}
